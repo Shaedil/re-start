343b70c66a47586ba5102dfb30003a9a2d671706
refact0r <34758569+refact0r@users.noreply.github.com>
2025-12-30 15:36:55 -0800

    fix styling

--- src/lib/components/RadioButton.svelte
+++ src/lib/components/RadioButton.svelte
@@ -32,18 +32,11 @@
         color: var(--txt-3);
     }
 
-    .radio-indicator:hover {
-        color: var(--txt-2);
-    }
-
     .radio-x {
         color: var(--txt-2);
     }
 
-    label:hover {
-        .radio-indicator::before,
-        .radio-indicator::after {
-            color: var(--txt-2);
-        }
+    label:hover .radio-indicator {
+        color: var(--txt-2);
     }
 </style>
